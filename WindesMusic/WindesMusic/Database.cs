608ba4cd3b578076a0098415e985b2ffbddcc936
Furkan <iamfurkanakmese@gmail.com>
2019-11-19 12:14:45 +0100

    Database code cleanup

--- WindesMusic/WindesMusic/Database.cs
+++ WindesMusic/WindesMusic/Database.cs
@@ -13,232 +13,162 @@ namespace WindesMusic
     {
         private string _provider;
         private string _connectionString;
-        private DbProviderFactory factory;
+        private DbProviderFactory _factory;
         private DbConnection _connection;
+        private DbCommand _command;
+        private DbDataReader _reader;
 
         public Database()
         {
             _provider = ConfigurationManager.AppSettings["provider"];
             _connectionString = ConfigurationManager.AppSettings["connectionString"];
-
-            factory = DbProviderFactories.GetFactory(_provider);
+            _factory = DbProviderFactories.GetFactory(_provider);
+            _connection = _factory.CreateConnection();
+            _command = _factory.CreateCommand();
+            _command.Connection = _connection;
         }
+
+        public void OpenConnection()
+        {
+            if (_connection == null)
+            {
+                System.Console.WriteLine("Connection problems");
+            }
+            _connection.ConnectionString = _connectionString;
+            _connection.Open();
+        }
+
         public void SetValues(string Query)
         {
-            using (_connection = factory.CreateConnection())
-            {
-                if (_connection == null)
-                {
-                System.Console.WriteLine("Connection problems");
-                }
-                _connection.ConnectionString = _connectionString;
+            OpenConnection();
+            _command.CommandText = Query;
+            _reader = _command.ExecuteReader();
 
-                _connection.Open();
-                DbCommand command = factory.CreateCommand();
-                List<int> results = new List<int>();
-
-                command.Connection = _connection;
-                command.CommandText = Query;
-                DbDataReader reader = command.ExecuteReader();
-
-                _connection.Close();
-            }
+            _connection.Close();
         }
+
         public List<int> GetRecordsInt(string Query, string ReturnItems)
         {
-            using (_connection = factory.CreateConnection())
+            OpenConnection();
+            List<int> results = new List<int>();
+            _command.CommandText = Query;
+            _reader = _command.ExecuteReader();
+
+            while (_reader.Read())
             {
-                if (_connection == null)
-                {
-                    return null;
-                }
-                _connection.ConnectionString = _connectionString;
-
-                _connection.Open();
-                DbCommand command = factory.CreateCommand();
-                List<int> results = new List<int>();
-
-                command.Connection = _connection;
-                command.CommandText = Query;
-
-                using (DbDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        results.Add((int)reader[$"{ReturnItems}"]);
-                    }
-                }
-                _connection.Close();
-                return results;
+                results.Add((int)_reader[$"{ReturnItems}"]);
             }
+            
+            _connection.Close();
+            return results;
         }
 
         public List<string> GetRecordsString(string Query, string ReturnItems)
         {
-            using (_connection = factory.CreateConnection())
+            OpenConnection();
+            List<string> results = new List<string>();
+            _command.CommandText = Query;
+            _reader = _command.ExecuteReader();
+
+            while (_reader.Read())
             {
-                if (_connection == null)
-                {
-                    return null;
-                }
-                _connection.ConnectionString = _connectionString;
-
-                _connection.Open();
-                DbCommand command = factory.CreateCommand();
-                List<string> results = new List<string>();
-
-                command.Connection = _connection;
-                command.CommandText = Query;
-
-                using (DbDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        results.Add((string)reader[$"{ReturnItems}"]);
-                    }
-                }
-                _connection.Close();
-                return results;
+                results.Add((string)_reader[$"{ReturnItems}"]);
             }
+            
+            _connection.Close();
+            return results;
         }
 
         
         // this function sends the login data to the database and returns a user object, empty if the data is wrong
         public User Login(string email, string password)
         {
-            using (_connection = factory.CreateConnection())
+            OpenConnection();
+            _command.Parameters.Clear();
+            User userResult = new User();    
+            _command.CommandText = "SELECT * FROM Users WHERE Email=@email AND Password=@password";
+
+            var emailParam = _command.CreateParameter();
+            emailParam.ParameterName = "@email";
+            emailParam.Value = email;
+
+            var sha1 = new SHA1CryptoServiceProvider();
+            var data = Encoding.ASCII.GetBytes(password);
+            var sha1data = sha1.ComputeHash(data);
+
+            var passwordParam = _command.CreateParameter();
+            passwordParam.ParameterName = "@password";
+            passwordParam.Value = Encoding.ASCII.GetString(sha1data);
+
+            _command.Parameters.Add(emailParam);
+            _command.Parameters.Add(passwordParam);
+            _reader = _command.ExecuteReader();
+
+            if (_reader.Read())
             {
-                if (_connection == null)
-                {
-                    System.Console.WriteLine("Connection problems");
-                }
-                _connection.ConnectionString = _connectionString;
-
-                _connection.Open();
-                DbCommand command = factory.CreateCommand();
-                User userResult = new User();    
-
-                command.Connection = _connection;
-                command.CommandText = "SELECT * FROM Users WHERE Email=@email AND Password=@password";
-
-                var emailParam = command.CreateParameter();
-                emailParam.ParameterName = "@email";
-                emailParam.Value = email;
-
-                var sha1 = new SHA1CryptoServiceProvider();
-                var data = Encoding.ASCII.GetBytes(password);
-                var sha1data = sha1.ComputeHash(data);
-
-                Console.WriteLine(Encoding.ASCII.GetString(sha1data));
-
-                var passwordParam = command.CreateParameter();
-                passwordParam.ParameterName = "@password";
-                passwordParam.Value = Encoding.ASCII.GetString(sha1data);
-                command.Parameters.Add(emailParam);
-                command.Parameters.Add(passwordParam);
-
-                DbDataReader reader = command.ExecuteReader();
-
-                if(reader.Read())
-                {
-                    userResult.Id = (int) reader["Id"];
-                    userResult.Email = (string) reader["Email"];
-
-                    // save user id to application settings
-                    Properties.Settings.Default.UserID = userResult.Id;
-                    Properties.Settings.Default.Save();
-                }
-                _connection.Close();
-                return userResult;
+                userResult.Id = (int)_reader["Id"];
+                userResult.Email = (string)_reader["Email"];
+                // save user id to application settings
+                Properties.Settings.Default.UserID = userResult.Id;
+                Properties.Settings.Default.Save();
             }
+            _connection.Close();
+            return userResult;
         }
-
+        // method for retrieving user data after login or on startup using ID
         public User GetUserData(int id)
         {
-            using (_connection = factory.CreateConnection())
+            OpenConnection();
+            _command.Parameters.Clear();
+            User userResult = new User();
+            _command.CommandText = "SELECT * FROM Users INNER JOIN Playlist ON Id=UserID WHERE Id=@id";
+
+            var idParam = _command.CreateParameter();
+            idParam.ParameterName = "@id";
+            idParam.Value = id;
+            _command.Parameters.Add(idParam);
+            _reader = _command.ExecuteReader();
+
+            while (_reader.Read())
             {
-                if (_connection == null)
-                {
-                    System.Console.WriteLine("Connection problems");
-                }
-                _connection.ConnectionString = _connectionString;
-
-                _connection.Open();
-                DbCommand command = factory.CreateCommand();
-                User userResult = new User();
-
-                command.Connection = _connection;
-                command.CommandText = "SELECT * FROM Users INNER JOIN Playlist ON Id=UserID WHERE Id=@id";
-
-                var idParam = command.CreateParameter();
-                idParam.ParameterName = "@id";
-                idParam.Value = id;
-
-                command.Parameters.Add(idParam);
-
-                DbDataReader reader = command.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        Playlist playlistResult = new Playlist();
-                        userResult.Id = (int)reader["Id"];
-                        userResult.Email = (string)reader["Email"];
-                        playlistResult.PlaylistID = (int) reader["PlaylistID"];
-                        playlistResult.PlaylistName = (string) reader["PlaylistName"];
-                        userResult.Playlists.Add(playlistResult);
-                    }
-                }
-
-                _connection.Close();
-                return userResult;
+                Playlist playlistResult = new Playlist();
+                userResult.Id = (int)_reader["Id"];
+                userResult.Email = (string)_reader["Email"];
+                playlistResult.PlaylistID = (int)_reader["PlaylistID"];
+                playlistResult.PlaylistName = (string)_reader["PlaylistName"];
+                userResult.Playlists.Add(playlistResult);
             }
+            _connection.Close();
+            return userResult; 
         }
 
+        // method to retieve search results
         public List<Song> GetSearchResults(string criteria)
         {
-            using (_connection = factory.CreateConnection())
+            OpenConnection();
+            _command.Parameters.Clear();
+            List<Song> listResult = new List<Song>();
+            _command.CommandText = "SELECT * FROM Song WHERE Name LIKE '%' + @criteria + '%' OR Artist LIKE '%' + @criteria + '%' OR Album LIKE '%' + @criteria + '%'";
+
+            var criteriaParam = _command.CreateParameter();
+            criteriaParam.ParameterName = "@criteria";
+            criteriaParam.Value = criteria;
+            _command.Parameters.Add(criteriaParam);
+            _reader = _command.ExecuteReader();
+
+            while (_reader.Read())
             {
-                if (_connection == null)
-                {
-                    System.Console.WriteLine("Connection problems");
-                }
-                _connection.ConnectionString = _connectionString;
-
-                _connection.Open();
-                DbCommand command = factory.CreateCommand();
-                List<Song> listResult = new List<Song>();
-
-                command.Connection = _connection;
-                command.CommandText = "SELECT * FROM Song WHERE Name LIKE '%' + @criteria + '%' OR Artist LIKE '%' + @criteria + '%' OR Album LIKE '%' + @criteria + '%'";
-
-                var criteriaParam = command.CreateParameter();
-                criteriaParam.ParameterName = "@criteria";
-                criteriaParam.Value = criteria.Trim();
-
-                command.Parameters.Add(criteriaParam);
-
-                DbDataReader reader = command.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        Song searchResult = new Song();
-                        searchResult.SongID = (int)reader["SongID"];
-                        searchResult.SongName = (string)reader["Name"];
-                        searchResult.Artist = (string)reader["Artist"];
-                        searchResult.Album = (string)reader["Album"];
-                        searchResult.Year = (int)reader["Year"];
-                        listResult.Add(searchResult);
-                    }
-                } else
-                {
-                    Console.WriteLine(criteria);
-                }
-
-                _connection.Close();
-                return listResult;
+                Song searchResult = new Song();
+                searchResult.SongID = (int)_reader["SongID"];
+                searchResult.SongName = (string)_reader["Name"];
+                searchResult.Artist = (string)_reader["Artist"];
+                searchResult.Album = (string)_reader["Album"];
+                searchResult.Year = (int)_reader["Year"];
+                listResult.Add(searchResult);
             }
+
+            _connection.Close();
+            return listResult; 
         }
     }
 }
